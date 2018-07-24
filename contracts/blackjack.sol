2cf8a0c001bb87c5d8413a38c618a2e61ff76750
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-07-24 16:11:28 -0400

    Update blackjack.sol

--- contracts/blackjack.sol
+++ contracts/blackjack.sol
@@ -1,1 +1,43 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.19;
+
+contract Casino{
+    
+    address player;
+    address bet_amount;
+    uint[] cards_opened;
+    
+    //encrypted dealer card, happpens off-chain 
+    bytes32[] dealer_cards;
+    uint minimum_bet;
+    uint max_wait;
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor(uint minimum_bet, uint max_wait) public {
+        owner = msg.sender;
+    }
+    
+    function _revealDealerCards() internal (uint) {
+        
+    }
+    
+    function placeBet() public payable {
+        
+    }
+    
+    function revealandStop() public  {
+        
+    }
+    
+    function viewPlayerCards() public view (uint total) {
+        
+    }
+    
+    function kill() public onlyOwner{
+    // destroy contract
+        selfdestruct(owner);
+    }
+}
